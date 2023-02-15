2c221549f41c93633e488360b8a8bd164727cc28
okayama <yuuki627.yuuki627@gmail.com>
2023-02-15 16:02:27 +0900

    [Add] index

---
+++ README.txt
@@ -0,0 +1,7 @@
+#このフォルダはGromacsの計算で使用するpythonプログラムが入っています
+
+acpype.py
+　→Amber形式からGromacs形式への変更
+
+mole_replace1(2,3,4).py
+　→topファイルにgmx genrestr で作成したitpファイルを追記して、拘束条件を付与させる
---
+++ mole_replace1.py
@@ -0,0 +1,19 @@
+import sys
+
+args = sys.argv
+
+file = args[1]
+before_str="[ moleculetype ]"
+after_str="#ifdef POSRES\n#include \"posre1.itp\"\n#endif\n\n[ moleculetype ]"
+
+n = 0
+with open(file) as f, open('cpx_solv_add-posre.top', 'w') as writer: 
+    for line in f:
+        if before_str in line:
+            n += 1
+        if n == 2:
+            line = line.replace(before_str, after_str)
+        writer.write(line)
+        
+
+
